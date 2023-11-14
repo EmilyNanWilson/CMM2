83ba428d046a1da55072c928afa0194f364f241a
EmilyNanWilson <149583516+EmilyNanWilson@users.noreply.github.com>
2023-11-14 14:11:02 +0000

    Add files via upload

--- Final_attempt.py
+++ Final_attempt.py
@@ -22,7 +22,7 @@ from scipy.integrate import solve_ivp
 # and tables of CL_el and CM_el vs delta_el 
 #--------------------------------------
 import sys
-sys.path.append('C:/Users/emily/OneDrive - University of Edinburgh/Third Year/Computational Methods/Design Project')
+sys.path.append('C:/Users/emily/OneDrive - University of Edinburgh/Third Year/Computational Methods/Design Project 2')
 import aero_table
 import env
 import vehicle
@@ -33,7 +33,7 @@ rho = env.air_density
 V = 100              #[m/s]
 S = vehicle.Sref
 W = vehicle.acMass *env.gravity          #[N]
-gamma = np.radians(2.864789)       #[rad]
+gamma = 0.05       #[rad]
 
 # Initial guesses for the fitting
 # i.e., initial values of a and b
@@ -49,15 +49,12 @@ def CL_a_func(x, a, b):
 # params contains the fitted values of a and b
 # params_covariance contains a measure of the achieved 
 # accuracy 
-params, params_covariance = optimize.curve_fit(CL_a_func, aero_table.alpha, aero_table.CL,
+params, params_covariance = optimize.curve_fit(CL_a_func, np.radians(aero_table.alpha), aero_table.CL,
         p0=[CL_0, CL_alpha])
 
 CL_0 = params[0]
 CL_alpha = params[1]
 #--------------------------------------
-
-x_values_CL_a = np.linspace(min(aero_table.alpha), max(aero_table.alpha), 10)
-y_values_CL_a = CL_a_func(x_values_CL_a,CL_0,CL_alpha)
 
 #--------------------------------------
 #Lift vs delta_elevator
@@ -66,15 +63,11 @@ CL_delta = 0.003
 def CL_d_func(x, a):
     return a * x
 
-params, params_covariance = optimize.curve_fit(CL_d_func, aero_table.delta_el, aero_table.CL_el,
+params, params_covariance = optimize.curve_fit(CL_d_func, np.radians(aero_table.delta_el), aero_table.CL_el,
         p0=[CL_delta])
 
 CL_delta = params[0]
 #--------------------------------------
-
-x_values_CL_d = np.linspace(min(aero_table.delta_el), max(aero_table.delta_el), 10)
-y_values_CL_d = CL_d_func(x_values_CL_d,CL_delta)
-
 
 #--------------------------------------
 # CD vs CL
@@ -90,8 +83,6 @@ params, params_covariance = optimize.curve_fit(CD_CL_func, aero_table.CL, aero_t
 CD_0 = params[0]
 CD_k = params[1]
 #--------------------------------------
-x_values_CD_CL = np.linspace(min(aero_table.CL), max(aero_table.CL), 10)
-y_values_CD_CL = CD_CL_func(x_values_CD_CL,CD_0,CD_k)
 
 
 #--------------------------------------
@@ -105,15 +96,12 @@ CM_alpha = -0.01
 def CM_alpha_func(x, a, b):
     return a + b * x
 
-params, params_covariance = optimize.curve_fit(CM_alpha_func, aero_table.alpha, aero_table.CM,
+params, params_covariance = optimize.curve_fit(CM_alpha_func, np.radians(aero_table.alpha), aero_table.CM,
         p0=[CM_0, CM_alpha])
 
 CM_0 = params[0]
 CM_alpha = params[1]
 
-x_values_CM_a = np.linspace(max(aero_table.alpha), min(aero_table.alpha), 10)
-
-y_values_CM_a = CM_alpha_func(x_values_CM_a, CM_0, CM_alpha)
 
 #--------------------------------------
 #Moment vs delta_elevator
@@ -124,13 +112,10 @@ CM_delta = -0.004
 def CM_delta_func(x, a):
     return a*x
 
-params, params_covariance = optimize.curve_fit(CM_delta_func, aero_table.delta_el, aero_table.CM_el, p0 = [CM_delta])
+params, params_covariance = optimize.curve_fit(CM_delta_func,np.radians( aero_table.delta_el), aero_table.CM_el, p0 = [CM_delta])
 
 CM_delta =params[0]
 
-#--------------------------------------
-x_values_CM_d = np.linspace(min(aero_table.delta_el), max(aero_table.delta_el), 10)
-y_values_CM_d = CM_delta_func(x_values_CM_d,CM_delta)
 
 #--------------------------------------
 # Write results on screen (check)
@@ -139,98 +124,65 @@ print("CD_0 ", CD_0, " CD_k ",CD_k)
 print("CM_0 ", CM_0, " CM_alpha ",CM_alpha, " CM_delta, ", CM_delta)
 #--------------------------------------
 
-#Plotting Results of A1
-def plot(x_data, y_data, x_label, y_label, title, x_fitted, y_fitted):
-    plt.figure()
-    plt.scatter(x_data, y_data, color='purple')
-    plt.plot(x_fitted, y_fitted, color='red')
-    plt.xlabel(x_label, fontsize=14)
-    plt.ylabel(y_label, fontsize=14)
-    plt.title(title, fontsize=16)
-    plt.show()
-
-plot(aero_table.alpha, aero_table.CL, 'alpha', 'CL_wing',
-          'CL_wing = C_L_0 + C_L_alpha*alpha', x_values_CL_a, y_values_CL_a)
-
-plot(aero_table.delta_el, aero_table.CL_el, 'delta_el', 'CL_el',
-          'CL_el = CL_d * delta_el', x_values_CL_d, y_values_CL_d)
-
-plot(aero_table.CL, aero_table.CD, 'CL', 'CD',
-          'CD = CD_0 + K*(CL^2)', x_values_CD_CL, y_values_CD_CL)
-
-plot(aero_table.alpha, aero_table.CM, 'alpha', 'CM_wing',
-          'CM_wing = CM_0 + CM_alpha*alpha', x_values_CM_a, y_values_CM_a)
-
-plot(aero_table.delta_el, aero_table.CM_el, 'delta_el', 'CM_delta',
-          'CM_el = CM_delta*delta_el', x_values_CM_d, y_values_CM_d)
 
 #----------------------
 # PART A2
 #----------------------
 
-def pitch_function(alpha,V,CM):
-    return 0.5*rho*(V**2)*S*CM
+
+
+def delta_function(alpha):
+    return -((CM_0 + (CM_alpha * alpha))  / CM_delta)
+
+def CL_function(alpha):
+    delta = delta_function(alpha) 
+    return CL_0 + (CL_alpha * alpha) + (CL_delta * delta)
+
+def CD_function(alpha):
+    CL = CL_function(alpha)
+    return CD_0 + (CD_k * (CL**2))
 
 def CM_function(alpha):
-    return CM_0 + (CM_alpha*alpha)+(CM_delta*delta)
-
-def delta_function(alpha): 
-    return (-((CM_0 + (CM_alpha*alpha))/CM_delta))
-
-def Cl_function(alpha): 
     delta = delta_function(alpha)
-    return CL_0 + (CL_alpha*alpha) + (CL_delta*delta)
-
-def lift_function(alpha):
-    Cl = Cl_function(alpha)
-    return 0.5*rho*(V**2)*S*Cl
-
-def CD_function(Cl): 
-    return CD_0 + (CD_k * (Cl**2))
-
-def drag_function(alpha):
-    CD = CD_function(alpha)
-    return 0.5*rho*(V**2)*S*CD
+    return CM_0 + (CM_alpha * alpha) + (CM_delta * delta)
 
 
 
 def A2(alpha, V, gamma):
-    
-    alpha = alpha[0]
-    alpha_deg = np.degrees(alpha)
-    Cl = Cl_function(alpha_deg)
-    CD = CD_function(Cl)
-    L = 0.5*rho*(V**2)*S*Cl
-    D = 0.5*rho*(V**2)*S*CD
-    return - (L * np.cos(alpha)) - (D * np.sin(alpha)) + (W * np.cos(alpha + gamma))
-
-alpha_solved = fsolve(A2, 0,args =  (V,gamma))
-
-alpha_deg = np.degrees(alpha_solved)
-print ("Alpha is:...... ", alpha_solved, "   [rad]")
+    CL = CL_function(alpha)
+    CD = CD_function(alpha)
+    L = 0.5 * env.air_density * V**2 * vehicle.Sref * CL
+    D = 0.5 * env.air_density * V**2 * vehicle.Sref * CD
+    W = vehicle.acMass * env.gravity
+    return -L * np.cos(alpha) - D * np.sin(alpha) + W * np.cos(alpha + gamma)
 
 
-def thrust_function(alpha_solved,gamma):
-    alpha_deg = np.degrees(alpha_solved)
-    Cl = Cl_function(alpha_deg)
-    CD = CD_function(Cl)
-    L = 0.5*rho*(V**2)*S*Cl
-    D = 0.5*rho*(V**2)*S*CD
-    return (vehicle.acMass * env.gravity) * math.sin(alpha_solved + gamma) + D * math.cos(alpha_solved) - L * math.sin(alpha_solved)
+alpha_radians = fsolve(A2, x0 = 0, args=(V, gamma))
+
+print ("Alpha is:...... ", alpha_radians, "   [rad]")
 
 
+delta_radians = delta_function(alpha_radians)
 
-delta = np.radians(delta_function(alpha_deg))
+CL = CL_0 + CL_alpha*alpha_radians + CL_delta * delta_radians
+CD = CD_0 + CD_k*(CL**2)
 
-Thrust = thrust_function(alpha_solved, gamma)
-#- lift_function(np.degrees(alpha_solved)) * np.sin(np.degrees(alpha_solved)) + drag_function(np.degrees(alpha_solved)) * np.cos(np.degrees(alpha_solved)) + W * np.sin(np.degrees(alpha_solved) + np.degrees(gamma))
+
+L = 0.5 * rho * V**2 * S * CL
+D = 0.5 * rho * V**2 * S * CD
+
+
+theta_radians = gamma + alpha_radians
+
+Thrust = -L*np.sin(alpha_radians)+D*np.cos(alpha_radians)+W*np.sin(theta_radians)
 q = 0
-theta = (delta) + (alpha_solved)
-u_b = V * np.cos(alpha_solved)
-w_b = V * np.sin(alpha_solved)
+theta = (gamma) + (alpha_radians)
+
+u_b = V * np.cos(alpha_radians)
+w_b = V * np.sin(alpha_radians)
 
 
-print('delta_e is', delta)
+print('delta_e is   ', delta_radians, "   [rad]")
 print('Thrust is', Thrust)
 print('q is', q)
 print('theta is', theta)
@@ -246,75 +198,53 @@ new_delta = 0
 
 #Part A3: define function for dub/dt
 
-def model(t,y):
-    if t>=100:
-        new_delta = (delta*1.1)*180/math.pi
-        pass
-    else:
-        new_delta = (delta)*180/math.pi
-        pass
-    '''
+def A3(t,y):
     y1 = y[0] # u_b
     y2 = y[1] # w_b
     y3 = y[2] # q
     y4 = y[3] # theta
     y5 = y[4] # Xe
     y6 = y[5] # Ze
-    alpha_deg = np.degrees(np.arctan(w_b/u_b))
-    V_new = np.sqrt(u_b**2 + w_b**2)
-    CM =  CM_0 + (CM_alpha*alpha_deg)+(CM_delta*new_delta)
-    Cl = CL_0 + (CL_alpha*alpha_deg) + (CL_delta*new_delta)
-    CD = CD_0 + (CD_k * (Cl**2))
-    L_new = 0.5*rho*(V_new**2)*S*Cl
-    D_new = 0.5*rho*(V_new**2)*S*CD
-    M_new = 0.5*rho*(V_new**2)*S*CM*vehicle.cbar
-    f1 = ((L_new/vehicle.acMass)*np.sin(alpha_deg))-((D_new/vehicle.acMass)*np.cos(alpha_deg))-(y3*y2-(W/vehicle.acMass)*np.sin(theta)) +(Thrust/vehicle.acMass)
-    f2 = ((-L_new/vehicle.acMass)*np.cos(alpha_deg))-((D_new/vehicle.acMass)*np.sin(alpha_deg))+(y3*y1)+((W/vehicle.acMass)*np.cos(theta))
-    f3 = (M_new/vehicle.inertia_yy)
-    f4 = y3
-    f5 = (y1*np.cos(y4))+(y2*np.sin(y4))
-    f6 = (-y1*np.sin(y4)) + (y2*np.cos(y4))
-    return f1,f2,f3,f4,f5,f6
-'''
-    u_b, w_b, theta,q, x_e,z_e = y
+    if t>=100:
+        delta_A3 = (delta_radians*1.1)
+        
+    else:
+        delta_A3 = (delta_radians)
+    
+    
+    thrust_A3 = Thrust
+    M = vehicle.acMass
+    W = env.gravity * M
+    
+    alpha_A3 = np.arctan(y2/y1)
+    V_A3 = np.sqrt((y1**2) + (y2**2))
+    
+    Cl_A3 = CL_0 + (CL_alpha*alpha_A3) + (CL_delta*delta_A3)
+    CM_A3 =  CM_0 + (CM_alpha*alpha_A3)+(CM_delta*delta_A3)
+    CD_A3 = CD_0 + (CD_k * (Cl_A3**2))
+    
+    L_A3 = 0.5*rho*(V_A3**2)*S*Cl_A3
+    D_A3 = 0.5*rho*(V_A3**2)*S*CD_A3
+    M_A3 = 0.5*rho*(V_A3**2)*S*CM_A3*vehicle.cbar
+    
 
-    alpha = np.arctan(w_b/u_b) 
-    V = np.sqrt(u_b**2 + w_b**2)
+    
+    y1_dt = ((L_A3 / M_A3) * np.sin(alpha_A3)) - ((D_A3 / M_A3) * np.cos(alpha_A3)) - (y3 * y2) - ((W / M_A3) * np.sin(y4)) + (thrust_A3 / M_A3)
+    y2_dt =  ((-L_A3 / M_A3) * np.cos(alpha_A3)) - ((D_A3 / M_A3) * np.sin(alpha_A3)) + (y3 * y1) + (W / M_A3) * np.cos(y4)
+    y3_dt = (M_A3/vehicle.inertia_yy)
+    y4_dt = y3
+    y5_dt = (y1 * np.cos(y4)) + (y2 * np.sin(y4))
+    y6_dt = -(y1 * np.sin(y4)) + (y2 * np.cos(y4))
+    return y1_dt,y2_dt,y3_dt,y4_dt,y5_dt,y6_dt
 
-    W = (vehicle.acMass * env.gravity)
-    S = vehicle.Sref
-    rho = env.air_density
-    cbar = vehicle.cbar
-
-    alpha_deg = np.degrees(alpha)
-    delta_deg = np.degrees(delta)
-
-    CL = CL_0 + (CL_alpha * alpha_deg) + (CL_delta * delta_deg)
-    CD = CD_0 + CD_k*(CL**2)
-    CM = CM_0 + (CM_alpha * alpha_deg) + (CM_delta * delta_deg)
-
-    L = 0.5 * rho * (V**2) * S * CL
-    D = 0.5 * rho * (V**2) * S * CD
-    M = 0.5 * rho * (V**2) * S * CM * cbar
-
-    dq_dt = (M/vehicle.inertia_yy)
-    dtheta_dt = q
-
-    du_dt = (L*math.sin(alpha) - D*math.cos(alpha) - vehicle.acMass*q*w_b - W*math.sin(theta) + Thrust)/vehicle.acMass
-    dw_dt = (-L*math.cos(alpha)-D*math.sin(alpha)+vehicle.acMass*q*u_b + W*math.cos(theta))/vehicle.acMass
-
-    dx_dt = u_b*math.cos(theta) + w_b*math.sin(theta)
-    dz_dt = - u_b*math.sin(theta) + w_b*math.cos(theta)
-
-    return du_dt,dw_dt,dtheta_dt,dq_dt,dx_dt,dz_dt
 
 t0 = 0
-y1_0 = 1.646 
-y2_0 = 99.986 
+y1_0 = u_b 
+y2_0 = w_b
 y3_0 = 0
-y4_0 = 0.0164
+y4_0 = theta_radians
 y5_0 = 0
-y6_0 = 0
+y6_0 = -2000
 
 t_final = 300
 
@@ -329,7 +259,7 @@ t_eval = np.linspace(t0,t_final,5000)
 # ------------------------------------------------------
 # Apply solve_ivp method
 
-soln1 = solve_ivp(model, [t0,t_final] , [u_b,w_b,theta,0,0,0])
+soln1 = solve_ivp(A3, [t0,t_final] , [y1_0,y2_0,y3_0,y4_0,y5_0,y6_0])
 y = soln1.y
 
 
